05526a2f7119f4c49778660b6278edd1808c6292
Stone <70432247+stone0118@users.noreply.github.com>
2022-08-28 22:11:48 +0800

    Update Block100.js

--- config/QuantumultX/Block100.js
+++ config/QuantumultX/Block100.js
@@ -2,7 +2,7 @@
 脚本功能：拦截100解锁永久会员
 [rewrite_local]
 # > 拦截100解锁永久超级会员
-^https?:\/\/tagit\.hyhuo\.com\/cypt\/block100\/get_vip_info$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/Block100FVIPCrack.js
+^https?:\/\/tagit\.hyhuo\.com\/cypt\/block100\/get_vip_info$ url script-response-body https://raw.githubusercontent.com/stone0118/Ruleset/main/config/QuantumultX/Block100.js
 [mitm] 
 hostname = tagit.hyhuo.com
 */
